b436b86d08aac9e5bac1d41db1c3ddd1d37b9f38
Samiran59 <Samiranm2@>
2025-06-24 14:26:33 +0530

    app upate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
-import Expirences from './components/expirences'
+import Expirences from './components/expirences';
 import Logos from './components/logos';
 import Testimonials from './components/testimonials-section';
 
